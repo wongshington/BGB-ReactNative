557c93551899b7c2b51a0df8b30bd8b945b98add
Carlos Garcia <carlosaicrag@gmail.com>
2020-11-13 19:02:46 -0800

    RN-01 create login options component

--- app/components/root.jsx
+++ app/components/root.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { NativeRouter } from 'react-router-native';
+// import { NativeRouter } from 'react-router-native';
 import App from "./app";
 import { Provider } from 'react-redux';
 
 const Root = ({store}) => {
   return(
     <Provider store={store}>
-        <NativeRouter>
+        {/* <NativeRouter> */}
           <App></App>
-        </NativeRouter>
+        {/* </NativeRouter> */}
       </Provider>
   )
 };
