e18a6a8b8f930541a2938356d14f2f00b8560fad
merhawit2019 <merhait@berkeley.edu>
2021-02-26 20:46:46 -0800

    added distance dropdown UI and aligned the two UI's horizontally

--- app/components/map/map_component.jsx
+++ app/components/map/map_component.jsx
@@ -7,6 +7,8 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import DropDownMenuProps from '@material-ui/core/MenuItem';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
 
 const MapComponent = ({navigation}) => {
   return (
@@ -16,18 +18,41 @@ const MapComponent = ({navigation}) => {
         title='Map'
       >
       </HeaderComponent>
-      <DropDownMenuProps>
-        <InputLabel>Topic</InputLabel>
-        <Select
-            labelId="topics"
-            id="topics"
+
+        <ToolbarComponent style={mapStyles.navigation} navigation={navigation}></ToolbarComponent>
+
+    <Grid container alignItems="center">
+
+        <DropDownMenuProps>
+            <InputLabel>Distance </InputLabel>
+            <Select
+                labelId="distance"
+                id="distance"
             >
-            <MenuItem >Topic 1</MenuItem>
-            <MenuItem >Topic 2</MenuItem>
-            <MenuItem >Topic 3</MenuItem>
-        </Select>
-      </DropDownMenuProps>
-      <ToolbarComponent style={mapStyles.navigation} navigation={navigation}></ToolbarComponent>
+                <MenuItem >0.5 miles</MenuItem>
+                <MenuItem >1.5 miles</MenuItem>
+                <MenuItem >2.5 miles</MenuItem>
+            </Select>
+        </DropDownMenuProps>
+
+        <Divider orientation="vertical" flexItem />
+
+          <DropDownMenuProps>
+            <InputLabel>Topic</InputLabel>
+            <Select
+                labelId="topics"
+                id="topics"
+                >
+                <MenuItem >Topic 1</MenuItem>
+                <MenuItem >Topic 2</MenuItem>
+                <MenuItem >Topic 3</MenuItem>
+            </Select>
+          </DropDownMenuProps>
+
+
+        </Grid>
+
+
     </View>
   )
 }
