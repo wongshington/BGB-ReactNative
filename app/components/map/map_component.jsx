2cedca56ddd7d4dcb8b2a1bbe4db9ad2cd34ff1c
Sarah Soares <sjsoares@berkeley.edu>
2021-03-17 16:37:29 -0700

    changing file structure

--- app/components/map/map_component.jsx
+++ app/components/map/map_component.jsx
@@ -3,14 +3,22 @@ import GoogleMapReact from 'google-map-react'
 import './map.css'
 import LocationPin from './LocationPin.jsx' 
 
-const Map = ({ location, zoomLevel }) => (
+const location = {
+  address: '1600 Amphitheatre Parkway, Mountain View, california.',
+  lat: 37.42216,
+  lng: -122.08427,
+}
+
+const MapComponent = ({ zoomLevel }) => {
+  return (
     <div className="map">
-      <h2 className="map-h2">Come Visit Us At Our Campus</h2>
+      <h2 className="map-h2">Look at our map!</h2>
       <div className="google-map">
         <GoogleMapReact
           bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
           defaultCenter={location}
-          defaultZoom={zoomLevel}
+          // defaultZoom={zoomLevel}
+          defaultZoom = {17}
         >
           <LocationPin
             lat={location.lat}
@@ -21,5 +29,5 @@ const Map = ({ location, zoomLevel }) => (
       </div>
     </div>
   )
-
-export default Map;
+}
+export default MapComponent;
