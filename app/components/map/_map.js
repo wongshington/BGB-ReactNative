be9dd58ce2c65b46b7a76b3ed91700d0e2b11d06
merhawit2019 <merhait@berkeley.edu>
2021-03-31 17:30:48 -0700

    the switching between map and resource is working

---
+++ app/components/map/_map.js
@@ -0,0 +1,24 @@
+
+import { StyleSheet } from "react-native";
+import { windowHeight, windowWidth } from "../../styles/styles";
+
+export const mapStyles = StyleSheet.create({
+    main: {
+        height: windowHeight,
+        width: windowWidth,
+        flex: 1,
+    },
+    navigation: {
+        height: windowHeight,
+        width: windowWidth,
+        flex: 1,
+        justifyContent: 'flex-end',
+        marginBottom: 36,
+    },
+    card: {
+        display: "flex",
+        justifyContent: "row",
+
+
+    }
+});
